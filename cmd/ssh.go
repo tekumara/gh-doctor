c0e3697c7dc924a8d9020e6ff301be79df4f16e8
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2025-10-17 21:13:45 +1100

    fix: freezing when running via ssh on windows

--- cmd/ssh.go
+++ cmd/ssh.go
@@ -450,7 +450,7 @@ func openBrowser(url string) error {
 	var cmd *exec.Cmd
 	switch runtime.GOOS {
 	case "windows":
-		cmd = exec.Command("cmd", "/c", "start", url)
+		cmd = exec.Command("rundll32", "url.dll,FileProtocolHandler", url)
 	case "darwin":
 		cmd = exec.Command("open", url)
 	default:
